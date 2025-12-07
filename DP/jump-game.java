8a3f855fb1e627e7309274e0ad8f618bb7c6a6fe
shujiejune <jieniu.june@tutanota.com>
2025-12-06 23:55:14 -0800

    jump game

---
+++ DP/jump-game.java
@@ -0,0 +1,5 @@
+/* Given an array of non-negative integers, you are initially positioned at the first index.
+ * Each element in the array represents the maximum jump length at that position.
+ * Determine if you are able to reach the last index.
+ */
+public boolean jumpGame(int[] array)
