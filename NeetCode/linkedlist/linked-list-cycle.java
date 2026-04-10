5f5f06b3806413a659ac03edbcd8f043961110ce
shujiejune <jieniu.june@tutanota.com>
2026-04-10 00:18:10 -0700

    feat: linkedlist easy

---
+++ NeetCode/linkedlist/linked-list-cycle.java
@@ -0,0 +1,14 @@
+class Solution {
+	public boolean hasCycle(ListNode head) {
+		ListNode fast = head;
+		ListNode slow = head;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+			if (slow == fast) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
